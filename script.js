5fdfe62bb68ae96f0feb0ca3f94ea473f96b5b80
Loang Chiang <loangchiangim@gmail.com>
2024-06-13 15:26:41 -0500

    Add full calculator functionality and improve styling

---
+++ script.js
@@ -0,0 +1,169 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    // when the user clicks on a number
+    document.querySelectorAll(".number").forEach(btn => {
+        btn.onclick = function() {
+            if (secondNum === false) {
+                number(btn, 1);
+            }
+            else {
+                number(btn, 2);
+            }
+        }
+    })
+
+    // when the user types in a number 
+    document.addEventListener('keydown', function(event) {
+        let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+        let key = event.key;
+
+        if (key in numbers) {  // check that the user is typing a number
+            if (secondNum === false) {
+                number(key, 1);
+            }
+            else {
+                number(key, 2);
+            }
+        }
+    });
+
+    // when the user clicks on an operation button
+    document.querySelectorAll(".symbol").forEach(btn => {
+        btn.onclick = function() {
+            if (num[1] !== "" && num[2] !== "") {  // checks that there are numbers to operate with
+                if (operation !== "") {  // completes the operation if user wants to operate for a second time
+                    equals("operation");
+                    operation = btn.dataset.val;
+                }
+            }
+            else {
+                operation = btn.dataset.val;
+                secondNum = true;
+            }
+        }
+    })
+
+    // when the user clicks on equals button 
+    document.querySelector("#bEquals").onclick = function() {
+        if (num[1] !== "" && num[2] !== "") {  // checks that there are numbers to operate with
+            equals("equals");
+        }
+    };
+
+    // when the user clears the screen
+    document.querySelector("#bAC").onclick = function() {
+        digitCount[1] = 0;
+        digitCount[2] = 0;
+        periodCount[1] = 0;
+        periodCount[2] = 0;
+        num[1] = "";
+        num[2] = "";
+        secondNum = false;
+        screen.innerHTML = num[1];
+    }
+
+    // when the user changes number sign from positive to negative and viceversa
+    document.querySelector("#bSign").onclick = function() {
+        if (!secondNum) {
+            changeSigns(1);
+        }
+        else {
+            changeSigns(2);
+        }  
+    }
+
+})
+
+
+// this is where my variables go
+    // when we move on to the second number
+    let secondNum = false;
+
+    let digitCount = {1: 0, 2: 0};
+    let periodCount = {1: 0, 2: 0};
+    let num = {1: "", 2: ""};
+
+    let operation = "";
+
+    let screen = document.querySelector("#screen");
+
+
+function number(btnOrKey, number) {
+    let value;
+
+    try {
+        value = btnOrKey.dataset.num;
+        console.log(`Adding btn ${value} to number ${number}`);
+    }
+    catch(err) {
+        value = btnOrKey;
+        console.log(`Adding key ${value} to number ${number}`);
+    }
+
+    digitCount[number] += 1;
+    // if there's room in the screen and this isn't a second period
+    if (digitCount[number] < 14 && !(periodCount[number] !== 0 && value === ".")) {
+        num[number] += value;
+        screen.innerHTML = num[number];
+        // if the user clicks on the period for the first time
+        if (value === ".") {
+            periodCount[number] += 1;
+        }
+    }
+}
+
+
+function changeSigns(number) {
+    console.log(`Changing signs for number ${number}`);
+
+    if (num[number][0] === "-") {  // if the number is already negative
+        num[number] = num[number].slice(1);  // deletes minus sign
+    }
+    else {
+        num[number] = "-" + num[number];  // adds minus sign
+    }
+    screen.innerHTML = num[number];
+}
+
+
+function equals(type) {  // type can be 'equals' for the equal sign and 'operation' for other symbols
+    console.log(`Running equals function! ${num[1]} ${operation} ${num[2]}`);
+
+    if (operation === "%") {
+        num[1] = parseFloat(num[1]) % parseFloat(num[2]);
+    }
+    else if (operation === "/") {
+        num[1] = parseFloat(num[1]) / parseFloat(num[2]);
+    }
+    else if (operation === "*") {
+        num[1] = parseFloat(num[1]) * parseFloat(num[2]);
+    }
+    else if (operation === "-") {
+        num[1] = parseFloat(num[1]) - parseFloat(num[2]);
+    }
+    else if (operation === "+") {
+        num[1] = parseFloat(num[1]) + parseFloat(num[2]);
+    }
+
+    // resets data
+    digitCount[2] = 0;
+    periodCount[2] = 0;
+    num[2] = "";
+    operation = "";
+
+    if (type === "equals") {  // if equals, we are on the first number again
+        secondNum = false;
+    }
+
+    if (num[1] === Infinity) {  // if the user is trying to divide by 0
+        screen.innerHTML = "silly";
+    }
+    else {
+        if (num[1].toString().length > 14) {  // if the number is too big for the screen
+            screen.innerHTML = num[1].toPrecision(9);
+        }
+        else {
+            screen.innerHTML = num[1];
+        }
+    }
+}
